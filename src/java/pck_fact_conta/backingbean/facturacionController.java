1e6b49a183f8091f57e9237a5127c233e1be3f35
AndiiBloo <aplopez04@gmail.com>
2017-06-15 20:49:58 -0500

    Creando BackingBeans

---
+++ src/java/pck_fact_conta/backingbean/facturacionController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pck_fact_conta.backingbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Andrés López
+ */
+@ManagedBean
+@SessionScoped
+public class facturacionController {
+
+    /**
+     * Creates a new instance of facturacionController
+     */
+    public facturacionController() {
+    }
+    
+}
